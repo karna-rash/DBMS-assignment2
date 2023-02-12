b391e60e4473777f265b711e92d55ff6053e0336
test <rutheeshreddychittepu@gmail.com>
2023-02-12 11:46:16 +0530

    just created a folder

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import Navbar from './Navbar'
+
+function Home(props) {
+
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home;
---
+++ frontend/src/components/Registerpage.js
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar'
+import '../App.css';
+import axios from 'axios'
+function Registerpage() {
+
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [regRes, setRegRes] = useState('0');
+
+  let handleSubmit = async (e) => {
+    setRegRes(-2);
+    e.preventDefault();
+
+    axios.post('http://localhost:5000/register', {
+      userName: userName,
+
+      pass: pass,
+      email: email
+    }).then((res) => {
+      setRegRes(res.data.regRes);
+
+
+    }).catch((err) => {
+      console.log(err);
+    });
+
+
+
+
+  };
+  return (
+
+    <div className="App">
+      <Navbar />
+      {
+        regRes == -2 && <p>Processing please wait.... Don't Click Button again</p>
+      }
+
+      {
+        regRes == 0 && <p>Enter inputs in below fields</p>
+      }
+      {regRes == 1 && navigate('/register/' + userName)}
+      {
+        regRes == -1 && <p>This username already taken, Enter a different username</p>
+      }
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userName}
+          placeholder="userame"
+          required
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={userName}
+          placeholder="display name"
+          required
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={pass}
+          placeholder="password"
+          required
+          onChange={(e) => setPass(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+
+
+
+      </form>
+    </div>
+
+  );
+}
+
+export default Registerpage;
