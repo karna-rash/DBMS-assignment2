4a97cc031c5fa028c8745be56072cf7d22459a46
test <rutheeshreddychittepu@gmail.com>
2023-02-13 19:41:45 +0530

    added rest client testing file test.http

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import User from '../models/users.js'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import * as url from 'url';
@@ -14,37 +13,43 @@ const router = express.Router();
 // Authentication Routes
 router.post('/login', async (req, res) => {
   console.log(req.body);
-  let users = await User.find({ username: req.body.userName }); //change this according to postgres
-  if (users.length == 0) {
-    res.sendStatus(406).json({ logRes: -1 });
-  }
-  else {
-    let user = users[0];
-    if (user.password == req.body.pass) {
+  // let users = await User.find({ username: req.body.userName }); //change this according to postgres
+  // if (users.length == 0) {
+  //   res.sendStatus(406).json({ logRes: -1 });
+  // }
+  // else {
+  //   let user = users[0];
+  //   if (user.password == req.body.pass) {
 
-      jwt.sign({
-        username: user.username,
-        email: user.email,
-        password: user.password
-      }, process.env.SECRET_KEY, (err, token) => {
-        if (err) { console.log(err); }
-        res.json({
-          token: token,
-          logRes: 1,
-          role:user.role,
-        })
-      })
-    }
+  //     jwt.sign({
+  //       username: user.username,
+  //       email: user.email,
+  //       password: user.password
+  //     }, process.env.SECRET_KEY, (err, token) => {
+  //       if (err) { console.log(err); }
+  //       res.json({
+  //         token: token,
+  //         logRes: 1,
+  //         role:user.role,
+  //       })
+  //     })
+  //   }
 
-    else {
-      res.json({ logRes: -2 });
-    }
-  }
+  //   else {
+  //     res.json({ logRes: -2 });
+  //   }
+  // }
+  res.json(
+    {
+      req:req.body
+    })
 });
 
-app.post('/register',async (req, res) => {
+router.post('/register',async (req, res) => {
   console.log(req.body)
-  
+  res.json({
+    req:req.body
+  })  
 
   });
 
