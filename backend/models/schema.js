36c4c418069e5544e85139b00076d06d91542acf
HarshaVardhanReddyG <cs21btech11017@iith.ac.in>
2023-02-15 16:12:10 +0530

    added conn.js

--- backend/models/schema.js
+++ backend/models/schema.js
@@ -1,11 +1,11 @@
-import client from '../config/database.js';
+import client from '../config/conn.js';
 
   var sql1="create table if not exists users ("+
-  " id integer, "+
+  " id serial primary key, "+
   " username varchar (40),"+
   " password varchar(30),"+
-  " display_name varchar(255),"+
-  " primary key (id))";
+  " display_name varchar(255) "+
+  " )";
 
 client.query(sql1, function (err, result) {
 if (err) throw err;
@@ -27,7 +27,7 @@ if (err) throw err;
   "creation_date TIMESTAMP NOT NULL, "+
   "last_access_date TIMESTAMP NOT NULL, "+
   "primary key (id), "+
-  "foreign key (id,display_name) references users "+
+  "foreign key (id) references users (id) on delete cascade "+
   ");";
 
  client.query(sql, function (err, result) {
@@ -39,6 +39,37 @@ var sqll2="CREATE table if not exists tags ( "+
 	" tag_name VARCHAR(255) NOT NULL "+
   " );"
   client.query(sqll2, function (err, result) {
+    if (err) console.error(err);
+  });
+
+var sqll3="create table if not exists posts( "+
+  "id serial primary key, "+
+  "Owner_id integer, "+
+  "OwnerName varchar(255), "+
+  "Title varchar(255), "+
+  "AnswerCount integer default 0, "+
+  "tags varchar(255), "+
+  "body text, "+
+  "creation_date timestamp, "+
+  "last_modified timestamp, "+
+  "foreign key (Owner_id) references users(id) "+
+  ");"
+  client.query(sqll3, function (err, result) {
+    if (err) console.error(err);
+  });
+
+var sql3="create table if not exists answers( "+
+  " id serial primary key, "+
+  " post_id integer, "+
+  " answeredby_id integer, "+
+  "down_votes INTEGER DEFAULT 0, "+
+  "up_votes INTEGER DEFAULT 0, "+
+  "creation_date TIMESTAMP NOT NULL, "+
+  "last_edited TIMESTAMP NOT NULL, "+
+  "foreign key (answeredby_id) references users(id), "+
+  "foreign key (post_id) references posts(id) "+
+  ");"
+  client.query(sql3, function (err, result) {
     if (err) console.error(err);
     else{
       client.end();
