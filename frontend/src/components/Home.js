8012dbd4803eb157f4824ba318cd189b398ad715
karna-rash <abhinay.sadineni@gmail.com>
2023-03-05 22:16:43 +0530

    homepage changed

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,18 +4,15 @@ import Navbar from './Navbar';
 const Home = () => {
   return (
     <div><Navbar/>
-    <div className="relative flex flex-col justify-center min-h-screen from-mycolour to-mycolour2 bg-gradient-115 overflow-hidden">
-    <div className="flex flex-col justify-center items-center rounded-lg bg-gray-100 mx-8">
-    <div className="w-1/2 h-72 mt-4 bg-register-image">
-              <h1 className="text-white text-3xl text-center mt-4">Welcome</h1>
-             
-            </div>
-      <main className="p-12">
-        <section id="about" className="py-12">
+    <div className="relative flex flex-col min-h-screen bg-no-repeat bg-cover justify-center bg-scroll" style={{backgroundImage: `url("https://lh3.googleusercontent.com/p/AF1QipN6SzIG0VC1OiXHjiV2Ru9ZCJMCd0YT1iPJ-pU=s680-w680-h510")`}}>
+    <div className="flex flex-col justify-center items-center rounded-lg mx-36">
+              <h1 className="text-black text-3xl text-center mt-4 mb-12">Welcome</h1>
+      <main className="p-12 flex flex-col bg-white">
+        <section id="about" className="py-10 mx-10">
           <h2 className="text-3xl font-bold text-indigo-500 text-center">About us</h2>
           <p className="text-gray-700">This is a Community Question Answer (CQA) website developed for the DBMS assignment. A CQA website is an online platform where users can ask questions on various topics and get answers from other members of the community. This website includes features such as tags, the ability to create, edit, or delete posts, search posts by tags or usernames, sorting by time, and upvoting, downvoting</p>
         </section>
-        <section id="contact" className="py-12 bg-gray-100">
+        <section id="contact" className="py-12 ">
   <h2 className="text-3xl font-bold text-indigo-500 text-center mb-8">Contact Us</h2>
   <div className='flex flex-col max-w-xl mx-auto'>
     <div className="mb-4">You can contact us at IITH or email us at:</div>
