6dee39c873748f9d39e6e1dff0db9dddcbee834c
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2023-03-05 21:04:48 +0530

    Homepage completed

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,34 +4,38 @@ import Navbar from './Navbar';
 const Home = () => {
   return (
     <div><Navbar/>
-    <div className="bg-gray-100 h-screen font-sans">
+    <div className="relative flex flex-col justify-center min-h-screen from-mycolour to-mycolour2 bg-gradient-115 overflow-hidden">
+    <div className="flex flex-col justify-center items-center rounded-lg bg-gray-100 mx-8">
+    <div className="w-1/2 h-72 mt-4 bg-register-image">
+              <h1 className="text-white text-3xl text-center mt-4">Welcome</h1>
+             
+            </div>
       <main className="p-12">
         <section id="about" className="py-12">
           <h2 className="text-3xl font-bold text-indigo-500 text-center">About us</h2>
-          <p className="text-gray-700">This is a Community Question Answer(CQA) website</p>
+          <p className="text-gray-700">This is a Community Question Answer(CQA) website developed for DBMS assignment. A CQA website is an online platform where users can ask questions on various topics and get answers from other members of the community. This website include features like tags, search post by tags, usernames, sorting by time, upvotes.</p>
         </section>
-        <section id="contact" className="py-12">
-          <h2 className="text-3xl font-bold text-indigo-500 text-center">Contact</h2>
-          <form className="text-gray-700" action="submit-form.php" method="post">
-            <div className="mb-4">
-              <label htmlFor="name" className="block font-bold mb-2">Name:</label>
-              <input type="text" id="name" name="name" className="w-full border border-gray-400 p-2 rounded-lg" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block font-bold mb-2">Email:</label>
-              <input type="email" id="email" name="email" className="w-full border border-gray-400 p-2 rounded-lg" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="message" className="block font-bold mb-2">Message:</label>
-              <textarea id="message" name="message" rows="5" className="w-full border border-gray-400 p-2 rounded-lg"></textarea>
-            </div>
-            <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600">Submit</button>
-          </form>
-        </section>
+        <section id="contact" className="py-12 bg-gray-100">
+  <h2 className="text-3xl font-bold text-indigo-500 text-center mb-8">Contact Us</h2>
+  <div className='flex flex-col max-w-xl mx-auto'>
+    <div className="mb-4">You can contact us at IITH or email us at:</div>
+    <div className="mb-2">
+      <span className="font-medium">cs21btech11014@iith.ac.in</span> (Chittepu Rutheesh Reddy)
+    </div>
+    <div className="mb-2">
+      <span className="font-medium">cs21btech11055@iith.ac.in</span> (Sadineni Abhinay)
+    </div>
+    <div className="mb-2">
+      <span className="font-medium">cs21btech11017@iith.ac.in</span> (G Harsha Vardhan Reddy)
+    </div>
+    <div className="mb-2">
+      <span className="font-medium">cs21btech11030@iith.ac.in</span> (Kotikalapudi Karthik)
+    </div>
+  </div>
+</section>
+
       </main>
-      <footer className="bg-black p-6">
-        <p className="text-white text-center">&copy;</p>
-      </footer>
+    </div>
     </div>
     </div>
   );
